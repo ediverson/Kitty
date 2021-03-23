d6e3d24217d6542c14acc99291dcc4f6edd5e435
Anton <ediverson@yandex.ru>
2021-03-23 21:23:05 +0300

    last task changed

--- Reactive Milk/Reactive Milk/Rocket task/RocketVC.swift
+++ Reactive Milk/Reactive Milk/Rocket task/RocketVC.swift
@@ -8,6 +8,9 @@ class RocketVC: UIViewController {
     @IBOutlet weak var launchButton: UIButton!
     @IBOutlet weak var infoLable: UILabel!
     
+    private var zapuskObs: Observable<Bool>!
+    private var launchObs: Observable<Bool>!
+    
     private let launchRocket = UIImage(named: "rocket")
     private let returnRocket = UIImage(named: "rocket 2")
     private let duration = 2.0
@@ -15,46 +18,84 @@ class RocketVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupButtons()
         setupVC()
     }
     
  //MARK: - Setup
+    
     private func setupVC() {
+        let mainObs = Observable<Bool>
+            .combineLatest(zapuskObs, launchObs) { (left, right) -> Bool in                         // комбинируем два нажатия
+                guard left == true && right == true else { return false }
+                ///print("l:", left, "r:", right)
+                return left && right                                                                // выводим результат в виде Bool
+            }
+            .share(replay: 2)
+        
+        mainObs
+            .do(onNext: {_ in
+                self.launchAnimation()                                                              // запускаем
+            })
+            .delay(RxTimeInterval.seconds(Int(duration)), scheduler: MainScheduler.instance)
+            .do(onNext: {_ in
+                self.returnAnimation()                                                              // возвращаем
+            })
+            .map { !$0 }                                                                            // меняем на обратное, чтобы отменить значение кнопок
+            .subscribe(onNext: {
+                ///print("binder", $0, "\n")
+                self.infoLable.text = "Ракета запущена"
+                self.launchButton.isSelected = $0                                                   // отменяем нажатие в свифте
+                self.zapuskButton.isSelected = $0
+            })
+            .disposed(by: bag)
+        
+        mainObs
+            .bind(to: launchButton.rx.isSelected, zapuskButton.rx.isSelected)                       // вероятнее всего проблема тут
+            .disposed(by: bag)                                                                      // мне кажется, что байнд срабатывает раньше, чем подписка
+    }
+    
+    
+    private func setupButtons() {
         
 // Кнопка "Запуск"
-        zapuskButton
+        zapuskObs = zapuskButton
             .rx
             .tap
-            .throttle(RxTimeInterval.seconds(Int(duration * 2)), scheduler: MainScheduler.instance)     // запрещаем много запусков
-            .do(onNext: {
-                self.launchAnimation()                                                                  // запускаем
-            })
-            .delay(RxTimeInterval.seconds(Int(duration)), scheduler: MainScheduler.instance)            // откладываем возвращение
-            .do(onNext: {
-                self.returnAnimation()                                                                  // возвращаем
-            })
-            .subscribe(onNext: {
-                self.infoLable.text = "Ракета запущена"                                                 // показываем лейбл
-            })
+            .map { el -> Bool in
+                ///print("запуск",self.zapuskButton.isSelected)
+                return !self.zapuskButton.isSelected                                                // изначально false, так что мы меняем на true
+            }
+            .map { el in                                                                            // тестовая функция
+                ///print("запуск",el, "\n")
+                return el
+            }
+            .share(replay: 2)
+
+        zapuskObs
+            .bind(to: self.zapuskButton.rx.isSelected)
             .disposed(by: bag)
 
 // Кнопка "Launch"
-        launchButton
+        launchObs = launchButton
             .rx
             .tap
-            .throttle(RxTimeInterval.seconds(Int(duration * 2)), scheduler: MainScheduler.instance)
-            .do(onNext: {
-                self.launchAnimation()
-            })
-            .delay(RxTimeInterval.seconds(Int(duration)), scheduler: MainScheduler.instance)
-            .do(onNext: {
-                self.returnAnimation()
-            })
-            .subscribe(onNext: {
-                self.infoLable.text = "Rocket launched"
-            })
+            .map { el -> Bool in
+                ///print("Launch", self.launchButton.isSelected)
+                return !self.launchButton.isSelected
+            }
+            .map { el in
+                ///print("Launch",el, "\n")
+                return el
+            }
+            .share(replay: 2)
+
+        launchObs
+            .bind(to: self.launchButton.rx.isSelected)
             .disposed(by: bag)
     }
+    
     
 //MARK: - Helpers
     private func moveUp(view: UIImageView) { view.center.y -= 300 }                                     // отправляем на
@@ -75,3 +116,25 @@ class RocketVC: UIViewController {
         animate(fun: moveDown, view: rocketImageView, image: returnRocket!)                              // посадка
     }
 }
+
+
+
+
+
+//Старый вариант
+//zapuskObs = zapuskButton
+//    .rx
+//    .tap
+//    .throttle(RxTimeInterval.seconds(Int(duration * 2)), scheduler: MainScheduler.instance)     // запрещаем много запусков
+//    .do(onNext: {
+//        self.launchAnimation()                                                                  // запускаем
+//    })
+//    .delay(RxTimeInterval.seconds(Int(duration)), scheduler: MainScheduler.instance)            // откладываем возвращение
+//    .do(onNext: {
+//        self.returnAnimation()                                                                  // возвращаем
+//    })
+//            .share(replay: 1)
+//            .subscribe(onNext: {
+//                self.infoLable.text = "Ракета запущена"                                                 // показываем лейбл
+//            })
+//            .disposed(by: bag)
