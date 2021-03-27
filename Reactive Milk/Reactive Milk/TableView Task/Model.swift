74f176d15d4048cf060c1e292d16fc8acf52cb18
Anton <ediverson@yandex.ru>
2021-03-28 01:07:23 +0300

    filter added

--- Reactive Milk/Reactive Milk/TableView Task/Model.swift
+++ Reactive Milk/Reactive Milk/TableView Task/Model.swift
@@ -7,18 +7,36 @@ class Names {
                                   "Глеб", "Николай", "Анатолий"]
     
     static func getName() -> String {
-        return(Names.names[Int.random(in: 0..<Names.names.count)])    // выдаем рандомное значение через .random
-                                                                      // в диапазоне от нуля до кол-ва элементов в массиве names
+        return (Names.names[Int.random(in: 0..<Names.names.count)])         // выдаем рандомное значение через .random
+                                                                            // в диапазоне от нуля до кол-ва элементов в массиве names
     }
-    
+            
     static func removeName(names: [String]) -> [String] {
         if names != [] {
-        var newNames = names                                          // создаем переменную
-        newNames.removeLast()                                         // удаляем последнее значение
-        return newNames                                               // возвращаем обновленный массив
+            var newNames = names                                            // создаем переменную
+            newNames.removeLast()                                           // удаляем последнее значение
+            return newNames                                                 // возвращаем обновленный массив
         } else {
-            return names                                              // если массив пустой, то удалять нечего
+            return names                                                    // если массив пустой, то удалять нечего
         }
+    }
+            
+    static func filterName(value: [String], el: String) -> [String] {
+        var filteredNames: [String] = []                                    // пустой массив
+        
+        if el != "" {                                                       // если в поиске не пусто
+            for name in value {                                             // проходимся по элементам массива
+                if name.contains(el){                                       // смотрим содержит ли элементы нужные нам данные
+                    filteredNames.append(name)                              // если да, то добавляем в новый массив
+                }
+            }
+            ///print("f", filteredNames)
+            return filteredNames                                            // возвращаем его
+        } else {                                                            // если строка пустая
+            ///print("v", value)
+            return value                                                    // возвращаем исходный массив
+        }
+        
     }
 }
 
--- Reactive Milk/Reactive Milk/TableView Task/TableView.swift
+++ Reactive Milk/Reactive Milk/TableView Task/TableView.swift
@@ -4,9 +4,11 @@ import UIKit
 
 class TableView: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
     
 // Default value
     private let names = BehaviorRelay(value: ["Кирилл", "Матвей", "Василий", "Яков"])
+    private var searchString = Observable.of("")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +17,7 @@ class TableView: UIViewController {
     override func viewDidAppear(_ animated: Bool) {                                                                     // чтобы таблица строилась
         super.viewDidAppear(true)                                                                                       // до появления на экране
         setupView()
+        setUpTF()
     }
     
 //MARK: - Setup view
@@ -34,11 +37,31 @@ class TableView: UIViewController {
             })
             .disposed(by: bag)
     }
+    
+    private func setUpTF(){
+        searchString = textField
+            .rx
+            .text
+            .orEmpty
+            .throttle(RxTimeInterval.seconds(2), scheduler: MainScheduler.instance)
+            .map { "\($0)" }
+            .skip(1)                                                                                                    // пропускаем при первом нажатии
+            .distinctUntilChanged()
+                                        /// фильтрации через .filter {  $0 != "" } нет, так как нам нужно получать пустую строку, когда пользователь задал что-то в поиске а потом стер
+        searchString
+            .map { el -> String in
+                self.names.accept(Names.filterName(value: self.names.value, el: el))                                    // вся логика в модели
+                return el                                                                                               // для красивого принта
+            }
+            .subscribe(onNext: { el in
+                print("Ищем: \(el)")                                                                                    // только для галочки
+            })
+            .disposed(by: bag)
+    }
 //MARK: - Create buttons
     @IBAction func addButton(_ sender: Any) {
         names.accept([Names.getName()] + names.value)                                                                   // BR получает новое значение
     }                                                                                                                   // и добавляет в него дефолное
-    
     
     @IBAction func deleteButton(_ sender: Any) {
         names.accept(Names.removeName(names: names.value))                                                              // BR принимает обновленный массив
